4d3092afb205605c2330b70d5dd8333ca5dffdac
Matt Bernhardt <mjbernha@mit.edu>
2015-06-23 15:52:39 -0400

    Adds more robust logging to form processing script - enumerates arrays
    Signed-off-by: Matt Bernhardt <mjbernha@mit.edu>

--- scripts/php/mail.php
+++ scripts/php/mail.php
@@ -37,7 +37,13 @@ if(count($_POST)){
 	// Log submitted values for everything but the tell us staff form (which is more strictly anonymous)
 	if ($strTemplateFile != 'tell-us-staff.html') {
 		foreach ($_REQUEST as $key => $value) {
-			$log->write('FORM: ' . $key . ": " . $value);
+			if (gettype($value) == "array") {
+				foreach ( $value as $term ) {
+					$log->write('FORM: ' . $key . ": " . $term);
+				}
+			} else {
+				$log->write('FORM: ' . $key . ": " . $value);
+			}
 		}
 	}
 
